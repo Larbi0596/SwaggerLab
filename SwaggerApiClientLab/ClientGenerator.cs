8e638225ef298242d28cc7967592934069e97334
Larbi Ait Djebara <alarbi.96@hotmail.fr>
2026-02-08 16:56:20 -0500

    commit

---
+++ SwaggerApiClientLab/ClientGenerator.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NSwag;
+using NSwag.CodeGeneration.CSharp;
+
+
+public class ClientGenerator
+{
+    public async Task GenerateClient()
+    {
+        using var httpClient = new HttpClient();
+        var swaggerJson = await httpClient.GetStringAsync("http://localhost:<port>/swagger/v1/swagger.json");
+        var document = await OpenApiDocument.FromJsonAsync(swaggerJson);
+
+        var settings = new CSharpClientGeneratorSettings
+        {
+            ClassName = "GeneratedApiClient",
+            CSharpGeneratorSettings = { Namespace = "MyApiClientNamespace" }
+        };
+
+        var generator = new CSharpClientGenerator(document, settings);
+        var code = generator.GenerateFile();
+
+        await File.WriteAllTextAsync("GeneratedApiClient.cs", code);
+    }
+}
---
+++ SwaggerApiClientLab/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        var app = builder.Build();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1:"));
+        app.MapControllers();
+        Task.Run(() => app.RunAsync());
+    }
+}
